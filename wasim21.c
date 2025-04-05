197d2061527fba7904fef4b31334a492ccaccd05
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-04-05 14:45:45 +0530

    Write a C program to calculate average of given number.

---
+++ wasim21.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter the value of a,b and c\n");
+    scanf("%d%d%d",&a,&b,&c);
+    printf("Average is=%f",(a+b+c)/3.0);
+    printf("\n");
+    return 0;
+}
