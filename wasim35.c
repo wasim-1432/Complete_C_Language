eb58d0b297980a55e32813e8af9903273797577b
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-04-05 15:19:53 +0530

    Write a C program to find number without last digit.

---
+++ wasim35.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter any number\n");
+    scanf("%d",&a);
+    printf("Without last digit is=%d",a/10);
+    printf("\n");
+    return 0;
+}
