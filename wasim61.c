2f7b8543c6620639c8eff4a10a14ebc81c36be8a
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-04-05 16:22:10 +0530

    Write a C program to assume price of 1 USD is 84.23.Write a program to take the amount in INR and convert it into USD.

---
+++ wasim61.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float INR;
+    printf("Enter the amount of INR\n");
+    scanf("%f",&INR);
+    printf("Required amount is=%.2f",INR/84.23);
+    printf("\n");
+    return 0;
+}
