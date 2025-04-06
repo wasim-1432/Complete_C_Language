d47116091af4a76cef44718e7cd2e6f6a4cad959
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-04-06 08:32:28 +0530

    Write a C program to print first 10 natural numbers in reverse order.

---
+++ wasim113.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i=10;
+    printf("Natural Number in reverse oredr:");
+    while(i)
+    {
+        printf("%d ",i--);
+    }
+    printf("\n");
+    return 0;
+}
