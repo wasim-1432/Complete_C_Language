65ae350c96b8fbc35aa8957d2d90a470683b9a6f
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-04-06 08:48:14 +0530

    Write a C program to print first 10 Even natural numbers.

---
+++ wasim116.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    printf("Even Natural Number is:");
+    while(i<=10)
+    {
+        printf("%d ",2*i++);
+    }
+    printf("\n");
+    return 0;
+}
