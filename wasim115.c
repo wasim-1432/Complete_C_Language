6eb5efbb3e125e246c2f3838cede58226cc73c12
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-04-06 08:46:55 +0530

    Write a C program to print first 10 odd narural numbers in reverse order.

---
+++ wasim115.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i=10;
+    printf("Odd Natural Number in reverse order:");
+    while(i)
+    {
+        printf("%d ",2*i---1);
+    }
+    printf("\n");
+    return 0;
+}
