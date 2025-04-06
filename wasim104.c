d93bb805434e3251122173f759b4db104f3a34b2
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-04-06 08:25:33 +0530

    Write a C program to take month number and print the month name.

---
+++ wasim104.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+int main()
+{
+    int month;
+    printf("Enter the month number\n");
+    scanf("%d",&month);
+    if(month==1)
+    {
+        printf("Jan");
+    }
+    else if(month==2)
+    {
+        printf("Feb");
+    }
+    else if(month==3)
+    {
+        printf("March");
+    }
+    else if(month==4)
+    {
+        printf("April");
+    }
+    else if(month==5)
+    {
+        printf("May");
+    }
+    else if(month==6)
+    {
+        printf("June");
+    }
+    else if(month==7)
+    {
+        printf("July");
+    }
+    else if(month==8)
+    {
+        printf("Aug");
+    }
+    else if(month==9)
+    {
+        printf("Sept");
+    }
+    else if(month==10)
+    {
+        printf("Oct");
+    }
+    else if(month==11)
+    {
+        printf("Nov");
+    }
+    else if(month==12)
+    {
+        printf("Dec");
+    }
+    printf("\n");
+    return 0;
+}
