c352c117a87687c4f517c4482427fcaa41aa0b13
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-04-06 10:00:14 +0530

    Write a C program to find digits in a given number.

---
+++ wasim142.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,count=0;
+    printf("Enter any number\n");
+    scanf("%d",&n);
+    while(n)
+    {
+        count++,n/=10;
+    }
+    printf("Digit in a given number=%d",count);
+    printf("\n");
+    return 0;
+}
