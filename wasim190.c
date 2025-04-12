e08206bc8df96d00e2b110920fd3c6658a9b6b62
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-04-12 08:06:57 +0530

    Write a C program for following pattern: ABCDCBA ABC CBA AB   BA A     A

---
+++ wasim190.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k;
+    for(i=1;i<=4;i++)
+    {
+        k='A';
+        for(j=1;j<=7;j++)
+        {
+            if(j<=5-i || j>=3+i)
+            {
+                printf("%c",k);
+                j<4?k++:k--;
+            }
+            else
+            {
+                printf(" ");
+                if(j==4)
+                {
+                    k--;
+                }
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+    return 0;
+}
