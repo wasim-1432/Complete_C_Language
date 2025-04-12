846239a5a6f0e7ad60d8c32da01628995a194ab9
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-04-12 14:26:46 +0530

    Write a C program to print N natural numbers.

---
+++ wasim214.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int Print_First_N_Natural_Numbers(int n);
+int main()
+{
+    int n;
+    printf("Enter any number\n");
+    scanf("%d",&n);
+    Print_First_N_Natural_Numbers(n);
+    printf("\n");
+}
+int Print_First_N_Natural_Numbers(int n)
+{
+    int i=1;
+    while(i<=n)
+    {
+        printf("%d ",i);
+        i++;
+    }
+}
